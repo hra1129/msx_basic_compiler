9ac3b18e972fb74c449305b62106dddba27dc928
HRA! <hra@mvj.biglobe.ne.jp>
2023-09-11 06:58:31 +0900

    Updated

---
+++ msx_basic_compiler/expressions/expression_csng.cpp
@@ -0,0 +1,24 @@
+// --------------------------------------------------------------------
+//	Expression
+// ====================================================================
+//	2023/July/29th	t.hara
+// --------------------------------------------------------------------
+#pragma once
+
+#include <string>
+#include <vector>
+#include "expression_csng.h"
+
+// --------------------------------------------------------------------
+void CEXPRESSION_CSNG::optimization( CCOMPILE_INFO *p_info ) {
+
+	this->p_operand->optimization( p_info );
+}
+
+// --------------------------------------------------------------------
+void CEXPRESSION_CSNG::compile( CCOMPILE_INFO *p_info ) {
+
+	this->p_operand->compile( p_info );
+	this->convert_type( p_info, CEXPRESSION_TYPE::SINGLE_REAL, this->p_operand->type );
+	this->type = CEXPRESSION_TYPE::SINGLE_REAL;
+}
---
+++ msx_basic_compiler/expressions/expression_fix.cpp
@@ -0,0 +1,59 @@
+// --------------------------------------------------------------------
+//	Expression
+// ====================================================================
+//	2023/July/29th	t.hara
+// --------------------------------------------------------------------
+#pragma once
+
+#include <string>
+#include <vector>
+#include "expression_fix.h"
+
+// --------------------------------------------------------------------
+void CEXPRESSION_FIX::optimization( CCOMPILE_INFO *p_info ) {
+
+	this->p_operand->optimization( p_info );
+}
+
+// --------------------------------------------------------------------
+void CEXPRESSION_FIX::compile( CCOMPILE_INFO *p_info ) {
+	CASSEMBLER_LINE asm_line;
+
+	//	æ‚Éˆø”‚ðˆ—
+	this->p_operand->compile( p_info );
+
+	if( this->p_operand->type == CEXPRESSION_TYPE::STRING ) {
+		p_info->errors.add( TYPE_MISMATCH, p_info->list.get_line_no() );
+		return;
+	}
+	p_info->assembler_list.add_label( "work_valtyp", "0x0f663" );
+	p_info->assembler_list.add_label( "work_dac", "0x0f7f6" );
+	p_info->assembler_list.add_label( "bios_fixer", "0x030be" );
+	if( this->p_operand->type == CEXPRESSION_TYPE::INTEGER ) {
+		this->type = CEXPRESSION_TYPE::INTEGER;
+	}
+	else if( this->p_operand->type == CEXPRESSION_TYPE::SINGLE_REAL ) {
+		p_info->assembler_list.add_label( "bios_frcsng", "0x02fb2" );
+		p_info->assembler_list.activate_ld_dac_single_real();
+		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "ld_dac_single_real", COPERAND_TYPE::NONE, "" );
+		p_info->assembler_list.body.push_back( asm_line );
+		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "bios_fixer", COPERAND_TYPE::NONE, "" );
+		p_info->assembler_list.body.push_back( asm_line );
+		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "bios_frcsng", COPERAND_TYPE::NONE, "" );
+		p_info->assembler_list.body.push_back( asm_line );
+		this->type = CEXPRESSION_TYPE::SINGLE_REAL;
+	}
+	else {
+		p_info->assembler_list.add_label( "bios_frcdbl", "0x0303a" );
+		p_info->assembler_list.activate_ld_dac_double_real();
+		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "ld_dac_double_real", COPERAND_TYPE::NONE, "" );
+		p_info->assembler_list.body.push_back( asm_line );
+		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "bios_fixer", COPERAND_TYPE::NONE, "" );
+		p_info->assembler_list.body.push_back( asm_line );
+		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "bios_frcdbl", COPERAND_TYPE::NONE, "" );
+		p_info->assembler_list.body.push_back( asm_line );
+		this->type = CEXPRESSION_TYPE::DOUBLE_REAL;
+	}
+	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::CONSTANT, "work_dac" );
+	p_info->assembler_list.body.push_back( asm_line );
+}
