0cec8012733823bc1e7a7c094587608c9985db3e
HRA! <hra@mvj.biglobe.ne.jp>
2023-11-11 13:39:05 +0900

    Modified
    BEEP を ROMver で MAINROM, SUBROM のどちらを呼ぶのか切り替えていたが、常に MAINROM の方を呼ぶように修正。

--- msx_basic_compiler/collections/beep.cpp
+++ msx_basic_compiler/collections/beep.cpp
@@ -19,30 +19,8 @@ bool CBEEP::exec( CCOMPILE_INFO *p_info ) {
 	}
 	p_info->list.p_position++;
 
-	p_info->assembler_list.add_label( "work_romver", "0x002D" );
 	p_info->assembler_list.add_label( "bios_beep", "0x00C0" );
-	p_info->assembler_list.add_label( "bsub_beep", "0x017D" );
-	p_info->assembler_list.add_label( "bios_extrom", "0x0015F" );
-	s_label1 = p_info->get_auto_label();
-	s_label2 = p_info->get_auto_label();
-
-	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::LABEL, "work_romver" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::OR, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::REGISTER, "A" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::JR, CCONDITION::NZ, COPERAND_TYPE::LABEL, s_label1, COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
 	asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "bios_beep", COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::JR, CCONDITION::NONE, COPERAND_TYPE::LABEL, s_label2, COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::LABEL, CCONDITION::NONE, COPERAND_TYPE::LABEL, s_label1, COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "IX", COPERAND_TYPE::LABEL, "bsub_beep" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "bios_extrom", COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
-	asm_line.set( CMNEMONIC_TYPE::LABEL, CCONDITION::NONE, COPERAND_TYPE::LABEL, s_label2, COPERAND_TYPE::NONE, "" );
 	p_info->assembler_list.body.push_back( asm_line );
 	return true;
 }
