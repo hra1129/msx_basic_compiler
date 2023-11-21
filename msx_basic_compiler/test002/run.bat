f274a26e1530eac17add5f9f639027badb4cdf23
HRA! <hra@mvj.biglobe.ne.jp>
2023-11-21 18:57:10 +0900

    Bugfix ATN() optimaizer

--- msx_basic_compiler/test002/run.bat
+++ msx_basic_compiler/test002/run.bat
@@ -1,5 +1,4 @@
-..\x64\release\msx_bacon.exe -zma -msx2 test.asc test.asm
-rem ..\x64\release\msx_bacon.exe -m80 -msx2 test.asc test.mac
+..\x64\release\msx_bacon.exe -zma -O3 -msx2 test.asc test.asm
 ..\zma.exe test.asm test.bin
 copy ..\baconloader\BACONLDR.BIN .
 pause
