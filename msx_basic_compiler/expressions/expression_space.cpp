6454864d0f6bbd8235f2534ac57d2895ce0244df
HRA! <hra@mvj.biglobe.ne.jp>
2023-12-04 22:36:32 +0900

    Added SPACE$()
    SPACE$ を追加しました。

---
+++ msx_basic_compiler/expressions/expression_space.cpp
@@ -0,0 +1,80 @@
+// --------------------------------------------------------------------
+//	Expression
+// ====================================================================
+//	2023/July/29th	t.hara
+// --------------------------------------------------------------------
+#include <string>
+#include <vector>
+#include "expression_space.h"
+#include "expression_term.h"
+
+// --------------------------------------------------------------------
+CEXPRESSION_NODE* CEXPRESSION_SPACE::optimization( CCOMPILE_INFO *p_info ) {
+	CEXPRESSION_NODE *p;
+
+	p = this->p_operand->optimization( p_info );
+	if( p != nullptr ) {
+		delete this->p_operand;
+		this->p_operand = p;
+	}
+	//	Ž–‘OŒvŽZˆ—
+	if( (p_info->options.optimize_level >= COPTIMIZE_LEVEL::DEEP) && this->p_operand->is_constant ) {
+		//	’è”‚Ìê‡
+		if( this->p_operand->type != CEXPRESSION_TYPE::STRING ) {
+			//	”’l‚Ìê‡
+			int n = std::stoi( this->p_operand->s_value );
+			CEXPRESSION_TERM *p_term = new CEXPRESSION_TERM();
+			p_term->type = CEXPRESSION_TYPE::STRING;
+			p_term->s_value = "";
+			for( int i = 0; i < n; i++ ) {
+				p_term->s_value = p_term->s_value + " ";
+			}
+			return p_term;
+		}
+	}
+	return nullptr;
+}
+
+// --------------------------------------------------------------------
+void CEXPRESSION_SPACE::compile( CCOMPILE_INFO *p_info ) {
+	CASSEMBLER_LINE asm_line;
+
+	//	æ‚Éˆø”‚ðˆ—
+	this->p_operand->compile( p_info );
+	this->p_operand->convert_type( p_info, CEXPRESSION_TYPE::INTEGER, this->p_operand->type );
+	this->type = CEXPRESSION_TYPE::STRING;
+
+	p_info->assembler_list.activate_allocate_string();
+	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::REGISTER, "L" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::LABEL, "allocate_string", COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "B", COPERAND_TYPE::MEMORY_REGISTER, "[HL]" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::INC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "B", COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::DEC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "B", COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::PUSH, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+
+	std::string s_skip_label = p_info->get_auto_label();
+	asm_line.set( CMNEMONIC_TYPE::JR, CCONDITION::Z, COPERAND_TYPE::LABEL, s_skip_label, COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+
+	std::string s_loop_label = p_info->get_auto_label();
+	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::CONSTANT, "' '" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::LABEL, CCONDITION::NONE, COPERAND_TYPE::LABEL, s_loop_label, COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::INC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::MEMORY_REGISTER, "[HL]", COPERAND_TYPE::REGISTER, "A" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::DJNZ, CCONDITION::NONE, COPERAND_TYPE::LABEL, s_loop_label, COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::LABEL, CCONDITION::NONE, COPERAND_TYPE::LABEL, s_skip_label, COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+	asm_line.set( CMNEMONIC_TYPE::POP, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::NONE, "" );
+	p_info->assembler_list.body.push_back( asm_line );
+}
---
+++ msx_basic_compiler/expressions/expression_space.h
@@ -0,0 +1,34 @@
+// --------------------------------------------------------------------
+//	Expression SPACE$
+// ====================================================================
+//	2023/July/29th	t.hara
+// --------------------------------------------------------------------
+#include <string>
+#include <vector>
+#include "expression.h"
+
+#ifndef __EXPRESSION_SPACE_H__
+#define __EXPRESSION_SPACE_H__
+
+// --------------------------------------------------------------------
+//	ŠÖ”ŒÄ‚Ño‚µ
+class CEXPRESSION_SPACE: public CEXPRESSION_NODE {
+public:
+	~CEXPRESSION_SPACE() {
+		this->release();
+	}
+	CEXPRESSION_NODE *p_operand = nullptr;
+
+	CEXPRESSION_NODE* optimization( CCOMPILE_INFO *p_info );
+
+	void compile( CCOMPILE_INFO *p_info );
+
+	void release( void ) {
+		if( this->p_operand != nullptr ) {
+			delete this->p_operand;
+			this->p_operand = nullptr;
+		}
+	}
+};
+
+#endif
