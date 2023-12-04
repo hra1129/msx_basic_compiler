64cfdb582c56df61623e8f45062d16afba61370a
HRA! <hra@mvj.biglobe.ne.jp>
2023-12-04 21:43:21 +0900

    Bugfix FRE() function, and Modified STACK at initialize process
    ・起動時にスタックの初期化方法を変更
    ・FRE() 関数が正常に動作していなかったバグを修正

--- msx_basic_compiler/expressions/expression_fre.cpp
+++ msx_basic_compiler/expressions/expression_fre.cpp
@@ -25,8 +25,12 @@ void CEXPRESSION_FRE::compile( CCOMPILE_INFO *p_info ) {
 	CASSEMBLER_LINE asm_line;
 
 	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::MEMORY_CONSTANT, "[heap_end]" );
+	p_info->assembler_list.body.push_back( asm_line );
 	asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "DE", COPERAND_TYPE::MEMORY_CONSTANT, "[heap_next]" );
+	p_info->assembler_list.body.push_back( asm_line );
 	asm_line.set( CMNEMONIC_TYPE::OR, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::REGISTER, "A" );
+	p_info->assembler_list.body.push_back( asm_line );
 	asm_line.set( CMNEMONIC_TYPE::SBC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::REGISTER, "DE" );
+	p_info->assembler_list.body.push_back( asm_line );
 	this->type = CEXPRESSION_TYPE::INTEGER;
 }
