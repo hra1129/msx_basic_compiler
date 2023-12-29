764a296d3c64bad2d5863871af35fc6136073b5f
HRA! <hra@mvj.biglobe.ne.jp>
2023-12-29 15:57:52 +0900

    Added GET TIME, GET DATE command, and bugfix
    ・GET TIME 文字列変数名 [,A] を追加
    ・GET DATE 文字列変数名 [,A] を追加
    ・LOCATE命令で出力コードが少しおかしかったバグを修正

--- msx_basic_compiler/collections/locate.cpp
+++ msx_basic_compiler/collections/locate.cpp
@@ -10,7 +10,7 @@
 // --------------------------------------------------------------------
 //  LOCATE <X>, <Y>, <CURSOR>
 bool CLOCATE::exec( CCOMPILE_INFO *p_info ) {
-	bool has_parameter;
+	bool has_x, has_y;
 	CASSEMBLER_LINE asm_line;
 	int line_no = p_info->list.get_line_no();
 	std::string s_label;
@@ -26,104 +26,143 @@ bool CLOCATE::exec( CCOMPILE_INFO *p_info ) {
 	p_info->assembler_list.add_label( "work_csrsw", "0x0FCA9" );
 	p_info->assembler_list.add_label( "work_prtflg", "0x0f416" );
 
-	asm_line.set( CMNEMONIC_TYPE::XOR, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::REGISTER, "A" );
-	p_info->assembler_list.body.push_back( asm_line );
-
 	CEXPRESSION exp;
 	//	第1引数 <X>
-	has_parameter = false;
-	if( exp.compile( p_info, CEXPRESSION_TYPE::INTEGER ) ) {
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "H", COPERAND_TYPE::REGISTER, "L" );
+	has_x = false;
+	if( exp.compile( p_info ) ) {
+		//	第1引数が存在した場合
+		asm_line.set( "LD", "", "H", "L" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::INC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "H", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "INC", "", "H" );
 		p_info->assembler_list.body.push_back( asm_line );
 		exp.release();
-		has_parameter = true;
+		has_x = true;
+
+		if( p_info->list.is_command_end() ) {
+			//	LOCATE X の場合
+			asm_line.set( "LD", "" "A", "[work_csry]" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "LD", "" "L", "A" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "CALL", "", "bios_posit" );
+			p_info->assembler_list.body.push_back( asm_line );
+			return true;
+		}
+
+		asm_line.set( "PUSH", "", "HL" );
+		p_info->assembler_list.body.push_back( asm_line );
 	}
 	else {
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::MEMORY, "[work_csry]" );
+		//	第1引数が省略されている場合
+		asm_line.set( "LD", "" "A", "[work_csrx]" );
 		p_info->assembler_list.body.push_back( asm_line );
-	}
-	if( p_info->list.is_command_end() ) {
-		if( !has_parameter ) {
-			//	LOCATE だけではエラー。
+		asm_line.set( "LD", "" "H", "A" );
+		p_info->assembler_list.body.push_back( asm_line );
+		if( p_info->list.is_command_end() ) {
+			//	LOCATE : 引数が全くない場合
 			p_info->errors.add( MISSING_OPERAND, p_info->list.get_line_no() );
+			return true;
 		}
-		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "bios_posit", COPERAND_TYPE::NONE, "" );
-		p_info->assembler_list.body.push_back( asm_line );
-		return true;
 	}
-	asm_line.set( CMNEMONIC_TYPE::PUSH, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
+
+	//	,
 	if( p_info->list.p_position->s_word != "," ) {
 		p_info->errors.add( MISSING_OPERAND, p_info->list.get_line_no() );
 		return true;
 	}
 	p_info->list.p_position++;
+
 	//	第2引数 <Y>
-	has_parameter = false;
-	if( exp.compile( p_info, CEXPRESSION_TYPE::INTEGER ) ) {
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::REGISTER, "L" );
-		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::INC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::NONE, "" );
-		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::POP, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::NONE, "" );
-		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "L", COPERAND_TYPE::REGISTER, "A" );
+	has_y = false;
+	if( exp.compile( p_info ) ) {
+		//	第2引数が存在した場合
+		if( !has_x ) {
+			//	LOCATE ,Y : 第1引数は省略されていた場合
+			asm_line.set( "LD", "" "A", "[work_csrx]" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "LD", "" "H", "A" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "INC", "" "L" );
+			p_info->assembler_list.body.push_back( asm_line );
+		}
+		else {
+			//	LOCATE X, Y : 第1も第2も存在した場合
+			asm_line.set( "LD", "", "A", "L" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "INC", "", "A", "" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "POP", "", "HL", "" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "LD", "", "L", "A" );
+			p_info->assembler_list.body.push_back( asm_line );
+		}
+		asm_line.set( "CALL", "", "bios_posit" );
 		p_info->assembler_list.body.push_back( asm_line );
 		exp.release();
-		has_parameter = true;
+		has_y = true;
 	}
 	else {
-		asm_line.set( CMNEMONIC_TYPE::POP, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "HL", COPERAND_TYPE::NONE, "" );
-		p_info->assembler_list.body.push_back( asm_line );
-	}
-	asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "bios_posit", COPERAND_TYPE::NONE, "" );
-	p_info->assembler_list.body.push_back( asm_line );
-	if( p_info->list.is_command_end() ) {
-		if( !has_parameter ) {
-			//	LOCATE x, のように第二引数までで第二引数が省略されている場合はエラー
-			p_info->errors.add( MISSING_OPERAND, p_info->list.get_line_no() );
+		if( !has_x ) {
+			//	LOCATE , : 第1引数も第2引数も省略されている場合
+			if( p_info->list.is_command_end() ) {
+				//	LOCATE , : 引数無しでカンマ1つで終わってる場合
+				p_info->errors.add( MISSING_OPERAND, p_info->list.get_line_no() );
+				return true;
+			}
+			//	LOCATE , , ... : カーソル位置指定のパラメータがないため、posit 呼び出しは省略
 		}
-		return true;
+		else {
+			//	LOCATE X, : 第2引数が省略されている場合
+			asm_line.set( "POP", "", "HL" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "LD", "" "A", "[work_csry]" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "INC", "" "A" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "LD", "" "L", "A" );
+			p_info->assembler_list.body.push_back( asm_line );
+			asm_line.set( "CALL", "", "bios_posit" );
+			p_info->assembler_list.body.push_back( asm_line );
+		}
 	}
-	if( p_info->list.p_position->s_word != "," ) {
-		p_info->errors.add( SYNTAX_ERROR, p_info->list.get_line_no() );
+
+	//	,
+	if( p_info->list.is_command_end() || p_info->list.p_position->s_word != "," ) {
 		return true;
 	}
 	p_info->list.p_position++;
 
 	//	第3引数 <カーソルスイッチ>
-	has_parameter = false;
-	if( exp.compile( p_info, CEXPRESSION_TYPE::INTEGER ) ) {
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::CONSTANT, "0x1B" );
+	has_x = false;
+	if( exp.compile( p_info ) ) {
+		asm_line.set( "LD", "", "A", "0x1B" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::RST, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "0x18", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "RST", "", "0x18", "" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::REGISTER, "0x78" );
+		asm_line.set( "LD", "", "A", "0x78" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::INC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "L", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "INC", "", "L", "" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::DEC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "L", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "DEC", "", "L", "" );
 		p_info->assembler_list.body.push_back( asm_line );
 		s_label = p_info->get_auto_label();
-		asm_line.set( CMNEMONIC_TYPE::JR, CCONDITION::Z, COPERAND_TYPE::CONSTANT, s_label, COPERAND_TYPE::NONE, "" );
+		asm_line.set( "JR", "Z", s_label, "" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::INC, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "INC", "", "A", "" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::LABEL, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, s_label, COPERAND_TYPE::NONE, "" );
+		asm_line.set( "LABEL", "", s_label, "" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::RST, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "0x18", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "RST", "", "0x18", "" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::CONSTANT, "0x35" );
+		asm_line.set( "LD", "", "A", "0x35" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::RST, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "0x18", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "RST", "", "0x18", "" );
 		p_info->assembler_list.body.push_back( asm_line );
 		exp.release();
-		has_parameter = true;
+		has_x = true;
 	}
 	if( p_info->list.is_command_end() ) {
-		if( !has_parameter ) {
+		if( !has_x ) {
 			//	LOCATE x,y, のように第三引数までで第三引数が省略されている場合はエラー
 			p_info->errors.add( MISSING_OPERAND, p_info->list.get_line_no() );
 		}
