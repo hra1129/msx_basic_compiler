c84008b5936ea736b0823ead527c5de92c221f7e
HRA! <hra@mvj.biglobe.ne.jp>
2024-02-08 07:14:16 +0900

    Bugfix ISSUE#6
    ISSUE#6 のバグ修正がおかしかったのを修正

--- msx_basic_compiler/expressions/expression_operator_minus.cpp
+++ msx_basic_compiler/expressions/expression_operator_minus.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <vector>
 #include "expression_operator_minus.h"
+#include "expression_term.h"
 
 // --------------------------------------------------------------------
 CEXPRESSION_NODE* CEXPRESSION_OPERATOR_MINUS::optimization( CCOMPILE_INFO *p_info ) {
@@ -18,6 +19,16 @@ CEXPRESSION_NODE* CEXPRESSION_OPERATOR_MINUS::optimization( CCOMPILE_INFO *p_inf
 	if( p != nullptr ) {
 		delete (this->p_right);
 		this->p_right = p;
+	}
+
+	if( this->p_right->is_constant ) {
+		//	項が両方とも定数の場合
+		if( this->p_right->type != CEXPRESSION_TYPE::STRING ) {
+			//	数値の場合
+			CEXPRESSION_TERM *p_term  = new CEXPRESSION_TERM();
+			p_term->set_double( -std::stod(p_right->s_value) );
+			return p_term;
+		}
 	}
 	return nullptr;
 }
@@ -42,18 +53,16 @@ void CEXPRESSION_OPERATOR_MINUS::compile( CCOMPILE_INFO *p_info ) {
 	p_info->assembler_list.add_label( "bios_vmovfm", "0x02f08" );
 	p_info->assembler_list.add_label( "bios_neg", "0x02e8d" );
 	p_info->assembler_list.add_label( "bios_frcsng", "0x0303a" );
-	if( this->p_right->type == CEXPRESSION_TYPE::INTEGER ) {
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::CONSTANT, "2" );
+	if( this->p_right->type == CEXPRESSION_TYPE::INTEGER ) {	//	-32768 のときにバグるけど・・・それは仕様です
+		asm_line.set( "EX", "", "DE", "HL" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::MEMORY, "[work_valtyp]", COPERAND_TYPE::MEMORY, "A" );
+		asm_line.set( "LD", "", "HL", "0" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::MEMORY, "[work_dac + 2]", COPERAND_TYPE::MEMORY, "HL" );
+		asm_line.set( "OR", "", "A", "A" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "bios_frcsng", COPERAND_TYPE::NONE, "" );
+		asm_line.set( "SBC", "", "HL", "DE" );
 		p_info->assembler_list.body.push_back( asm_line );
-		asm_line.set( CMNEMONIC_TYPE::CALL, CCONDITION::NONE, COPERAND_TYPE::CONSTANT, "bios_neg", COPERAND_TYPE::NONE, "" );
-		p_info->assembler_list.body.push_back( asm_line );
-		this->type = CEXPRESSION_TYPE::SINGLE_REAL;
+		this->type = CEXPRESSION_TYPE::INTEGER;
 	}
 	else if( this->p_right->type == CEXPRESSION_TYPE::SINGLE_REAL ) {
 		asm_line.set( CMNEMONIC_TYPE::LD, CCONDITION::NONE, COPERAND_TYPE::REGISTER, "A", COPERAND_TYPE::CONSTANT, "4" );
